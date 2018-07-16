80496a2faf72cfb1146bd9fc0af1c7e973246df5
Mark Chmarny <mark@chmarny.com>
2018-07-16 12:04:29 -0700

    Cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# advanced
-kubectl delete -f advanced-deploy/stage4.yaml --ignore-not-found=true
-kubectl delete -f advanced-deploy/stage3.yaml --ignore-not-found=true
-kubectl delete -f advanced-deploy/stage2.yaml --ignore-not-found=true
-kubectl delete -f advanced-deploy/stage1.yaml --ignore-not-found=true
+# blue/green
+kubectl delete -f blue-green-deploy/stage4.yaml --ignore-not-found=true
+kubectl delete -f blue-green-deploy/stage3.yaml --ignore-not-found=true
+kubectl delete -f blue-green-deploy/stage2.yaml --ignore-not-found=true
+kubectl delete -f blue-green-deploy/stage1.yaml --ignore-not-found=true
 
 # bind
 kubectl delete -f event-bind/bind.yaml --ignore-not-found=true
 
 # service
-kubectl delete -f service-deploy/app.yaml --ignore-not-found=true
+kubectl delete -f image-deploy/app.yaml --ignore-not-found=true
 
 # src to url
 kubectl delete -f src-to-url/app.yaml --ignore-not-found=true
